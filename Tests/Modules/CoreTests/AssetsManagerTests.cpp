7cd0345a663290c8cf4ce950f504e31734b5ad2b
PiotrPatelski <patelnia@DESKTOP-Q9EQQSL.localdomain>
2022-12-13 21:03:00 +0100

    [namefix] changed AssetsManagerTests incorrect starting upper case in test names

--- Tests/Modules/CoreTests/AssetsManagerTests.cpp
+++ Tests/Modules/CoreTests/AssetsManagerTests.cpp
@@ -15,28 +15,28 @@ struct MainMenuAssetsManagerTest : public testing::Test
 
 };
 
-TEST_F(MainMenuAssetsManagerTest, MainMenuTextureThrowsWhenPathIsInvalid)
+TEST_F(MainMenuAssetsManagerTest, mainMenuTextureThrowsWhenPathIsInvalid)
 {
     auto sut = std::make_unique<MainMenuAssetsManager>();
     sut->setBuildPath("Invalid/Test/Path");
     EXPECT_THROW(sut->fetchTextureFromFile(), std::runtime_error);
 }
 
-TEST_F(MainMenuAssetsManagerTest, MainMenuTextureIsSetCorrectlyWhenValidPathIsProvided)
+TEST_F(MainMenuAssetsManagerTest, mainMenuTextureIsSetCorrectlyWhenValidPathIsProvided)
 {
     auto sut = std::make_unique<MainMenuAssetsManager>();
     sut->setBuildPath(TEST_PATH);
     ASSERT_NO_THROW(sut->fetchTextureFromFile());
 }
 
-TEST_F(MainMenuAssetsManagerTest, MainMenuFontThrowsWhenPathIsInvalid)
+TEST_F(MainMenuAssetsManagerTest, mainMenuFontThrowsWhenPathIsInvalid)
 {
     auto sut = std::make_unique<MainMenuAssetsManager>();
     sut->setBuildPath("Invalid/Test/Path");
     EXPECT_THROW(sut->fetchFontFromFile(), std::runtime_error);
 }
 
-TEST_F(MainMenuAssetsManagerTest, MainMenuFontIsSetCorrectlyWhenValidPathIsProvided)
+TEST_F(MainMenuAssetsManagerTest, mainMenuFontIsSetCorrectlyWhenValidPathIsProvided)
 {
     auto sut = std::make_unique<MainMenuAssetsManager>();
     sut->setBuildPath(TEST_PATH);
