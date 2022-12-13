bf742da2cfa8b417bcdc67b735db13f3bcb5380a
PiotrPatelski <patelnia@DESKTOP-Q9EQQSL.localdomain>
2022-12-13 20:28:33 +0100

    [cmake + scripts] added setup for new Gui component

---
+++ Tests/Modules/GuiTests/ButtonTests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <Button.hpp>
+
+namespace Gui
+{
+
+struct ButtonTest : public testing::Test
+{
+    
+};
+
+TEST_F(ButtonTest, dummy)
+{
+
+}
+
+}
